4378b76a8b904d40dd2ecae193561ee7b1516283
Ritik-Sharma38 <rdsharma3883@gmail.com>
2020-03-12 15:02:32 +0530

    dnx

--- depression/app/App.js
+++ depression/app/App.js
@@ -1,78 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {SignedIn} from './navigators/SignedIn';
-import {SignedOut} from './navigators/SignedOut';
-import {fetchUser} from './actions/authAction'
-function App(){
-    const dispatch = useDispatch();
-    useEffect(()=>{
-        dispatch(fetchUser())
-    })
-    const Stack = createStackNavigator();
-    const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
-    return (
-            <NavigationContainer>
-                {isAuthenticated ? <SignedIn/> : <SignedOut/>}
-            </NavigationContainer>
-    );
-  }
-export default App;
+import React, {Component} from 'react';
+import { View } from 'react-native';
+import Counter from './ad.js';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-{/**import React, { Component } from 'react';
-import { StyleSheet, Text, View, StatusBar} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-
-import Depression from './app/Depression';
-import Loading from './app/Welcome';
-import AppDiscription from './app/AppDiscription';
-import Avtar from './app/Avtar';
-
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-
-const Stack = createStackNavigator();
-
-const reducer = () => {
-  return
+const initialState = {
+  count: 0
 }
 
-const store = createStore(reducer)
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case "INC": 
+      return {
+        count: state.count + 1
+      };
+    case "DEC":
+      return {
+        count: state.count - 1
+      }
+    default: 
+        return state
+  }
+  return state;
+}
 
-export default function App( { naivgation } ) {
+const store = createStore(reducer);
+
+export default class App extends React.Component {
+  render() {
     return (
       <Provider store={store}>
-        <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown : false}}>
-          <Stack.Screen
-            name="Welcome"
-            component={Loading}
-          />
-          <Stack.Screen
-            name="AppDiscription"
-            component={AppDiscription}
-          />
-          <Stack.Screen
-            name="Depression"
-            component={Depression}
-          />
-          <Stack.Screen
-            name="SelectYourAvtar"
-            component={Avtar}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
+        <Counter/>
+      </Provider>
     );
-}
-
-const styles = StyleSheet.create({
-  container: {
-   flexGrow: 1,
-  },
-
-});**/}
-
-
+  }
+} 
