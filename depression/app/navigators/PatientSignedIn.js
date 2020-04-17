c4423598529a86096b2a1b57e525b13a118d24b6
sr1jan <srofficialsingh@gmail.com>
2020-04-17 23:55:52 +0530

    add survey screens

--- depression/app/navigators/PatientSignedIn.js
+++ depression/app/navigators/PatientSignedIn.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import ProfileScreen from '../screens/profile/patient';
-import Video from '../screens/video';
-import AvtarSelection from "../screens/avatar";
-import Test from '../screens/test';
-import TalkToDoctor from '../screens/PatientScreens/TalkToDoctor';
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import ProfileScreen from '../screens/profile/patient'
+import Video from '../screens/video'
+import AvtarSelection from '../screens/avatar'
+import Test from '../screens/test'
+import TalkToDoctor from '../screens/PatientScreens/TalkToDoctor'
+import SurveyCompletedScreen from '../screens/Survey/SurveyComplete'
+import SurveyScreen from '../screens/Survey/SurveyScreen'
 
-const Drawer = createDrawerNavigator();
+const Drawer = createDrawerNavigator()
 
-export const PatientSignedIn =()=> (
-    <Drawer.Navigator
-        initialRouteName="Profile"
-        screenOptions={{
-            header: () => null,
-        }}>
-        <Drawer.Screen name="Profile" component={ProfileScreen} />
-        <Drawer.Screen name="Video" component={Video} />
-        <Drawer.Screen name="Avatar" component={AvtarSelection} />
-        <Drawer.Screen name="VideoCall" component={Test} />
-        <Drawer.Screen name="TalkToDoctor" component={TalkToDoctor} />
-    </Drawer.Navigator>
-);
-
+export const PatientSignedIn = () => (
+  <Drawer.Navigator
+    initialRouteName="Profile"
+    screenOptions={{
+      header: () => null
+    }}>
+    <Drawer.Screen name="Profile" component={ProfileScreen} />{' '}
+    <Drawer.Screen name="Video" component={Video} />{' '}
+    <Drawer.Screen name="Avatar" component={AvtarSelection} />{' '}
+    <Drawer.Screen name="VideoCall" component={Test} />{' '}
+    <Drawer.Screen name="TalkToDoctor" component={TalkToDoctor} />{' '}
+    <Drawer.Screen name="SurveyScreen" component={SurveyScreen} />
+    <Drawer.Screen name="SurveyComplete" component={SurveyCompletedScreen} />
+  </Drawer.Navigator>
+)
